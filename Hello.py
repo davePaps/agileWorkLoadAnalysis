0c11cd347c2bb2a1abd3a6fceb09bcd064f15cc0
davePaps <39386577+davePaps@users.noreply.github.com>
2026-06-10 10:34:46 +0000

    Streamlit updates

--- Hello.py
+++ Hello.py
@@ -1,7 +1,19 @@
 
 import pandas as pd
+import streamlit as st
 print("Hello World");
 
 df = pd.read_csv("20260609_Data.csv");
 print(df.describe());
 print(df.head());
+
+# manipulate dataframe
+# Group by county hub, return RA
+
+df.groupby("Your County Hub")["RA"].sum();
+
+st.title("Agile Data")
+st.write(df.head());
+st.dataframe(df);
+
+
